2fa8dd2e4c59ee250edb31e3a29276ceed14f475
Austin Murdock <austin.d.murdock@gmail.com>
2020-03-13 13:47:03 -0700

    Fix: type casing did not match eslint conventions

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 module.exports = {
-	rules: {
-		'body-leading-blank': [1, 'always'],
-		'footer-leading-blank': [1, 'always'],
-        'header-max-length': [2, 'always', 72],
-        'scope-empty': [2, 'always'],
-		'subject-case': [
-			2,
-			'never',
-			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-		],
-		'subject-empty': [2, 'never'],
-        'subject-full-stop': [2, 'never', '.'],
-		'type-case': [2, 'always', 'lower-case'],
-        'type-empty': [2, 'never'],
-        'type-enum': [2, 'always', [
-            'fix',
-            'update',
-            'new',
-            'breaking',
-            'docs',
-            'build',
-            'upgrade',
-            'chore',
-        ]]
-	}
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 72],
+    'scope-empty': [2, 'always'],
+    'subject-case': [2, 'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'pascal-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always',
+      [ 'Fix', 'Update', 'New', 'Breaking', 'Docs', 'Build', 'Upgrade', 'Chore' ]
+    ]
+  }
 };
